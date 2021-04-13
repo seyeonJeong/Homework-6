aae03b09e4d0675979b11738e49b6e4dba77b6f5
seyeonJeong <wjdtpdus828@naver.com>
2021-04-13 14:30:21 +0900

    Update homework6.c

--- homework6.c
+++ homework6.c
@@ -56,7 +56,8 @@ int main()
 		printf("----------------------------------------------------------------\n");
 
 		printf("Command = ");
-		scanf_s(" %c", &command);
+		fflush(stdout);
+		scanf(" %c", &command);
 
 		switch (command) {
 		case 'z': case 'Z':
@@ -67,17 +68,20 @@ int main()
 			break;
 		case 'i': case 'I':
 			printf("Your Key = ");
-			scanf_s("%d", &key);
+			fflush(stdout);
+			scanf("%d", &key);
 			insertNode(headnode, key);
 			break;
 		case 'd': case 'D':
 			printf("Your Key = ");
-			scanf_s("%d", &key);
+			fflush(stdout);
+			scanf("%d", &key);
 			deleteNode(headnode, key);
 			break;
 		case 'n': case 'N':
 			printf("Your Key = ");
-			scanf_s("%d", &key);
+			fflush(stdout);
+			scanf("%d", &key);
 			insertLast(headnode, key);
 			break;
 		case 'e': case 'E':
@@ -85,7 +89,8 @@ int main()
 			break;
 		case 'f': case 'F':
 			printf("Your Key = ");
-			scanf_s("%d", &key);
+			fflush(stdout);
+			scanf("%d", &key);
 			insertFirst(headnode, key);
 			break;
 		case 't': case 'T':
@@ -155,6 +160,51 @@ int insertFirst(headNode* h, int key) {
 /* 리스트를 검색하여, 입력받은 key보다 큰값이 나오는 노드 바로 앞에 삽입 */
 int insertNode(headNode* h, int key)
 {
+	listNode* cur = h->first;//탐색을 위해 cur이라는 노드를 가리키는 포인터를 생성하고 리스트의 first를 가리키게함
+	listNode* stocur = NULL; // cur의 이전 노드를 기억하는 노드를 가리키는 포인터를 생성
+	listNode* n1 = (listNode*)malloc(sizeof(listNode));//동적할당을 이용하여 n1노드를 생성
+	n1->key = key; // n1노드의 key값에 매개변수로 받은 key값을 대입
+
+	if (h->first == NULL) //만약 헤더노드가 가리키는 노드가 NULL이라면(노드가 없음)
+	{
+		h->first = n1; // 헤더노드가 n1노드를 가리키게하고
+		n1->link = NULL; // n1이 NULL을 가리키게함 (n1을 첫번째 노드로 설정해줌)
+		return 0;
+	}
+	else if (h->first->key >= n1->key) //헤더노드가 가리키는 노드의 key 값이 n1의 key값보다 크거나 같은경우
+	{
+
+		n1->link = h->first; // n1이 가리키는 값이 헤더노드가 가리키는 노드가됨.
+		h->first = n1; // 헤더노드가 가리키는 값이 n1노드가 됨.
+		return 0;
+	}
+
+	while (cur != NULL)//cur이 NULL일때까지 반복
+	{
+		if (cur->key > n1->key) // 만약 cur의 key값이 n1의 key값보다 큰경우
+		{
+
+			n1->link = cur; //n1노드가 가리키는 노드가 cur이됨.
+			stocur->link = n1; //n1노드가 가리키는 노드가 cur이됨.
+			return 0;
+		}
+		if (cur->key == n1->key) // 만약 cur의 key값이 n1의 key 값과 같은경우
+		{
+
+			n1->link = cur; //n1노드가 가리키는 노드가 cur이됨.
+			stocur->link = n1; //n1노드가 가리키는 노드가 cur이됨.
+			return 0;
+		}
+
+
+		stocur = cur; //위의 두 if문의 조건에 만족하지 않는다면 cur노드를 이전노드로 설정
+		cur = cur->link; //cur노드를 cur이 가리키던 노드로 설정
+
+
+	}
+	n1->link = cur; // cur이 NULL이라면 (끝까지 탐색했을 경우이므로 n1의 key값이 가장 큰값!) n1이 가리키는 값이 cur 즉, NULL 즉, 마지막 노드가 된다.
+	stocur->link = n1; // cur의 이전값이 가리키는 노드가 n1이 됨.
+
 
 
 	return 0;
